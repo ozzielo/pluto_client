d7b647186b1014636527c0748cb81522770c43cd
ozzielo <57558395+ozzielo@users.noreply.github.com>
2022-12-14 00:43:22 -0500

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import { createRoot } from 'react-dom/client';
 
+import { MainView } from "./components/main-view/main-view";
+
 import "./index.scss";
 
 
 const PlutoBracketApplication = () => {
     return (
-        <div className="my-flix">
-            <div>Good morning</div>
-        </div>
+
+        <MainView />
+
     );
 };
 
